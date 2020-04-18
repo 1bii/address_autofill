16ea779f4cb32675895df56ebf1be8e8e1fca9a0
1bii <tan.him.iong@gmail.com>
2020-04-18 16:27:28 -0700

    Updates

---
+++ precache-manifest.d520b2d9668a194737170cda52230f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001366d89496336d52eb41ed42c9005d",
+    "url": "/address_autofill/index.html"
+  },
+  {
+    "revision": "7fa6e9468d19686795d3",
+    "url": "/address_autofill/static/css/main.f66334bd.chunk.css"
+  },
+  {
+    "revision": "5b28764e99857805f3b0",
+    "url": "/address_autofill/static/js/2.2037317f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/address_autofill/static/js/2.2037317f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fa6e9468d19686795d3",
+    "url": "/address_autofill/static/js/main.a713ea85.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "/address_autofill/static/js/main.a713ea85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ebc8d41a1c9ad347c80",
+    "url": "/address_autofill/static/js/runtime-main.66703af5.js"
+  }
+]);
